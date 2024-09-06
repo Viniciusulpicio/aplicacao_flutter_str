77757f903330b4aa1459ffbb5d62a8b6a75d0395
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-09-06 16:33:40 -0300

    alterações na tela de login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,48 +1,55 @@
-import 'package:flutter/material.dart'; // importa o flutter precisa em todas paginas 
+import 'package:flutter/material.dart'; // importa o flutter precisa em todas páginas
 
- class Login extends StatelessWidget { // digite stl para criar o template
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold( // scaffold é o celular em si, vai tudo dentro dele
-        backgroundColor:  Colors.black,// deixa o background cinza (para por essas cores precisa do Colors.cor)
-      appBar: AppBar( // voce cria o app bar (ou nav bar)
-        backgroundColor:  Colors.black,// deixa o background cinza (para por essas cores precisa do Colors.cor)
-
-         title: const Column(
-           children: [
-                   SizedBox(height: 22), // Adiciona um espaçamento de 16 pixels acima do título
-              Center(
-              child: Text("Entrar no twitter", style: TextStyle(
-                               color: Colors.white, // Define a cor do texto como branco
-                            ),),
-                     ),
-           ],
-           
-         ),), 
-
-        body: Align(
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 20, 31, 40),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(200.0), // Define a altura desejada
+          child: AppBar(
+            backgroundColor: const Color.fromARGB(255, 20, 31, 40),
+            flexibleSpace: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Centraliza os elementos
+              children: [
+               const SizedBox(height: 20), // Espaçamento superior
+                const Text(
+                  'Seja Bem-vindo ao Bluesky',
+                  style: TextStyle(
+                    color: Colors.white, // Cor do texto
+                    fontSize: 24, // Tamanho da fonte
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20), // Espaçamento entre o texto e a imagem
+                Image.asset(
+                  'asset/bluesky.png', // Verifique o caminho da imagem
+                  height: 100, // Altura da imagem
+              ),
+            ],
+          ),
           
-            alignment: Alignment.bottomCenter,
+            centerTitle: true, // Para garantir que o título esteja centralizado
+          ),
+        ),
+        body: Align(
+          // alignment: Alignment.bottomCenter,
           child: Container(
-            padding: const EdgeInsets.all(16.0), // padding para deixar uma margin por fora
-            margin: const EdgeInsets.all(8.0), // margin por fora
+            padding: const EdgeInsets.all(16.0), // padding interno
+            margin: const EdgeInsets.all(8.0), // margin externa
             height: 550,
-            decoration: 
-            BoxDecoration(
-              color: Colors.black, 
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 20, 31, 40),
               borderRadius: BorderRadius.circular(8),
             ),
-            
-          
             child: Form(
-              child: Column( // cria um filho para deixar tudo em forma de coluna
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Image(image: Image.asset("twitter.png")),
                   TextFormField(
                     autofocus: true,
                     decoration: const InputDecoration(
@@ -50,69 +57,67 @@ import 'package:flutter/material.dart'; // importa o flutter precisa em todas pa
                       hintText: "Informe o e-mail: ",
                       hintStyle: TextStyle(color: Colors.white), // Cor do texto do hint
                       focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white), // Cor da borda quando o campo está focado
+                        borderSide: BorderSide(color: Colors.white), // Cor da borda focada
                       ),
                     ),
                     style: const TextStyle(color: Colors.white), // Cor do texto digitado
                   ),
-
-                    const SizedBox(height: 4,),
-                    TextFormField(
+                  const SizedBox(height: 4),
+                  TextFormField(
                     autofocus: false,
                     decoration: const InputDecoration(
                       icon: Icon(Icons.password, color: Colors.white),
-                      hintText: "Informe a senha: ",hintStyle: TextStyle(color: Colors.white), // Cor do texto do hint
+                      hintText: "Informe a senha: ",
+                      hintStyle: TextStyle(color: Colors.white), // Cor do texto do hint
                       focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white), // Cor da borda quando o campo está focado
+                        borderSide: BorderSide(color: Colors.white), // Cor da borda focada
                       ),
                     ),
                     style: const TextStyle(color: Colors.white), // Cor do texto digitado
                   ),
-                  const SizedBox(height: 16,), // LINHA IMPORTANTE para dar margin e espaçamento é sizebox (é como se fosse o "br" do html)
+                  const SizedBox(height: 16), // Espaçamento
                   Center(
-                    child: ElevatedButton(onPressed: (){},  // cria outro botão
-                    
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(200, 50), // altera o tamanho do botao
-                      backgroundColor: Colors.blueAccent, // altera a cor de fundo
-                      foregroundColor: Colors.white, // altera a cor da letra
-                      shape: RoundedRectangleBorder( // faz o shape 
-                        borderRadius: BorderRadius.circular(10.0), // determina que o border radius sera 0.0 ou seja quadrado
+                    child: ElevatedButton(
+                      onPressed: () {}, // Ação do botão
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(200, 50), // Tamanho do botão
+                        backgroundColor: Colors.blueAccent, // Cor de fundo
+                        foregroundColor: Colors.white, // Cor do texto
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0), // Borda arredondada
+                        ),
+                        side: const BorderSide(
+                          color: Colors.blue, // Cor da borda
+                          width: 4.0, // Largura da borda
+                        ),
+                        elevation: 5, // Sombra do botão
+                        shadowColor: Colors.white.withOpacity(0.1), // Opacidade da sombra
                       ),
-                      
-                      side: const BorderSide( // altera o border
-                        color: Colors.blue, // muda a cor
-                        width: 4.0, // tamanho
-                      ),
-                      elevation: 5, 
-                      shadowColor: Colors.black.withOpacity(0.1),  // opacidade da borda
-                    ),child: const Text("Entrar"), // cria um filho dentro do botão e põe o texto
+                      child: const Text("Entrar"), // Texto no botão
                     ),
                   ),
-              
-                  const SizedBox(height: 16,),
-                  const Divider(color: Colors.white,),
-                    const Text(
-                      "Cadastrar",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.white, // Define a cor do texto como branco
-                      ),
+                  const SizedBox(height: 16),
+                  const Divider(color: Colors.white), // Linha divisória
+                  const Text(
+                    "Cadastrar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.white, // Cor do texto
                     ),
-
-                    const Text(
-                      "Sem cadastro? Clique acima",
-                      style: TextStyle(
-                        color: Colors.white, // Define a cor do texto como branco
-                      ),
+                  ),
+                  const Text(
+                    "Sem cadastro? Clique acima",
+                    style: TextStyle(
+                      color: Colors.white, // Cor do texto
                     ),
-
+                  ),
                 ],
               ),
             ),
           ),
         ),
+      ),
     );
   }
 }
