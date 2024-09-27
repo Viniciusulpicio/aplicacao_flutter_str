63edd46f27b8cadc983c55946e514d46856bc0dc
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-09-27 19:02:43 -0300

    atualizando splash screan

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:primeira_aplicacao/pages/login.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(milliseconds: 3515), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const Login()),
-      );
-    });
-
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity, // Preencher toda a largura
-        height: double.infinity, // Preencher toda a altura
-        child: Image.asset(
-          'assets/image/inicio.gif',
-          fit: BoxFit.cover, // Garantir que o GIF preencha a tela
-        ),
-      ),
+    return FlutterSplashScreen.gif(
+      gifPath: 'assets/image/inicio.gif',
+      gifWidth: double.infinity,
+      gifHeight: double.infinity,
+      nextScreen: const Login(),
+      duration: const Duration(milliseconds: 11000),
+      onInit: () {
+        debugPrint("Splash Screen: Initialized");
+      },
+      onEnd: () {
+        debugPrint("Splash Screen: Ended");
+      },
     );
   }
 }
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:primeira_aplicacao/pages/login.dart';
+
+// class Splash extends StatelessWidget {
+//   const Splash({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Future.delayed(const Duration(milliseconds: 9350), () {
+//       Navigator.of(context).pushReplacement(
+//         MaterialPageRoute(builder: (context) => const Login()),
+//       );
+//     });
+
+//     return Scaffold(
+//       body: SizedBox(
+//         width: double.infinity, // Preencher toda a largura
+//         height: double.infinity, // Preencher toda a altura
+//         child: Image.asset(
+//           'assets/image/inicio.gif',
+//           fit: BoxFit.cover, // Garantir que o GIF preencha a tela
+//         ),
+//       ),
+//     );
+//   }
+// }
